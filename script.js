10fd1ed32c8a47e8a519fe84b0b4e5eb0af6d8d0
Donutask <51319000+Donutask@users.noreply.github.com>
2024-08-07 15:29:08 +1000

    Count words and lines, support file drop

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const input = document.getElementById("input");
 const characterDisplay = document.getElementById("characters");
+const wordDisplay = document.getElementById("words");
+const lineDisplay = document.getElementById("lines");
 
-function CountCharacters() {
+// After this many characters, disable features to not crash computer
+const lagLimit = 50000;
+
+async function CountCharacters() {
     // Character Count
-    let c = input.value.length;
+    const v = input.value;
+    const c = v.length;
+
+    ReduceLag(c);
+
     // Plural or singular
-    let word = `Character${c == 1 ? '' : 's'}`;
+    const word = `Character${c == 1 ? '' : 's'}`;
     //On page
-    characterDisplay.innerHTML = `<b id='number'>${c}</b> ${word}`;
+    characterDisplay.innerHTML = `<b class='number'>${c}</b> ${word}`;
 
     //Title
     if (c <= 0) {
@@ -15,6 +24,85 @@ function CountCharacters() {
     } else {
         document.title = `${c} ${word}`;
     }
+
+    // Count words and lines, but only under a certain amount of characters
+    if (c < lagLimit) {
+        requestIdleCallback(() => {
+            const words = CountWords(v);
+            wordDisplay.innerHTML = `<b class=number>${words}</b> Word${words == 1 ? "" : "s"}`
+
+            const lines = CountLines(v);
+            lineDisplay.innerHTML = `<b class=number>${lines}</b> Line${lines == 1 ? "" : "s"}`
+        })
+    } else {
+        wordDisplay.innerHTML = "";
+        lineDisplay.innerHTML = "";
+    }
 }
+
+//Thanks: https://stackoverflow.com/a/37493957/13657726
+function CountWords(str) {
+    const m = str.match(/[^\s]+/g)
+    return m ? m.length : 0;
+}
+
+function CountLines(str) {
+    //No text = no lines
+    if (str.length <= 0) {
+        return 0;
+        //Otherwise count line break character
+    } else {
+        return lines = (String(str).match(/\n/g) || '').length + 1;
+    }
+}
+
+// With a lot of characters, disable TextArea functionality to improve performance (hoperfully)
+function ReduceLag(charCount) {
+    if (charCount > lagLimit) {
+        input.autocomplete = "no";
+        input.autocorrect = "no";
+        input.autocapitalize = "no";
+        input.spellcheck = false;
+
+    } else {
+        input.autocomplete = "yes";
+        input.autocorrect = "yes";
+        input.autocapitalize = "yes";
+        input.spellcheck = "default";
+    }
+}
+
+//Source: https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+function dropHandler(ev) {
+    // Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        [...ev.dataTransfer.items].forEach((item, i) => {
+            // If dropped items aren't files, reject them
+            if (item.kind === "file") {
+                const file = item.getAsFile();
+                ReadFile(file);
+            }
+        });
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        [...ev.dataTransfer.files].forEach((file, i) => {
+            ReadFile(file);
+        });
+    }
+}
+
+// Something to do with blobs
+async function ReadFile(file) {
+    const text = await file.text();
+
+    input.value += text;
+    CountCharacters();
+}
+
+
+input.addEventListener('input', () => CountCharacters());
 
 CountCharacters();
